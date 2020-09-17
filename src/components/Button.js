8d4ad357c587e6badc0da82e3510e84b94feb99d
robin <robin@nssplindia.com>
2020-09-17 20:30:07 +0530

    Initial Commit

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  render() {
+    return (
+      <div>
+        <p>Button</p>
+      </div>
+    );
+  }
+}
+
+export default Button;
