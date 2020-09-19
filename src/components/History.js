8dbb03bc24a4001bb9c1d9452576a5fa894b0ebb
robin <robin@nssplindia.com>
2020-09-20 00:40:36 +0530

    History scroller working

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,31 @@
 import React from "react";
 import "../Css/History.css";
 
+function isOverflowedX(element) {
+  return element.scrollWidth > element.clientWidth;
+}
+
+function isOverflowedY(element) {
+  return element.scrollHeight > element.clientHeight;
+}
+
+function addScrollbarIfNeeded(element) {
+  if (isOverflowedY(element)) element.style.overflowY = "scroll";
+  else element.style.overflowY = "initial";
+
+  if (isOverflowedX(element)) element.style.overflowX = "scroll";
+  else element.style.overflowX = "initial";
+}
 class History extends React.Component {
+  constructor(props) {
+    super(props);
+    this.historyList = React.createRef();
+  }
+
+  componentDidUpdate() {
+    addScrollbarIfNeeded(this.historyList.current);
+  }
+
   render() {
     const historyClassName = this.props.isShowHistory
       ? "history"
@@ -9,7 +33,7 @@ class History extends React.Component {
 
     return (
       <div className={historyClassName}>
-        <div className="history-list">
+        <div className="history-list" ref={this.historyList}>
           {this.props.history.map((item, index) => {
             return (
               <div key={index} className="history-item">
@@ -27,6 +51,14 @@ class History extends React.Component {
               </div>
             );
           })}
+        </div>
+        <div className="bottom-btns">
+          <button id="clear-history" onClick={this.props.onClearHistory}>
+            Clear
+          </button>
+          <button id="calculate" onClick={this.props.onEqual}>
+            Equal
+          </button>
         </div>
       </div>
     );
